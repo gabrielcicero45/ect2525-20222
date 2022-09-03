a86844f0a0c2caaf6aa587ab3d7666ab211a48f4
labinfo <labinfo@ectufrn.ect.ufrn.br>
2022-09-02 23:34:19 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
 export default function App() {
+  const bgImg = { uri: "https://img.freepik.com/vetores-gratis/fundo-de-estilo-comico-de-velocidade-azul_23-2148820924.jpg" };
+  const ditto = {uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"}
+  const pikachu = {uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/female/25.png"}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={bgImg} resizeMode="cover" style={styles.image}>
+      <Image source={ditto} style={styles.pokemon}></Image>
+      <Image source={pikachu} style={styles.pokemon}></Image>
+      <Text style={styles.text}>Ditto</Text>
       <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +20,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  pokemon:{
+    flex: 2,
+    height:10,
+    width:"100%",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 48,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  }
 });
